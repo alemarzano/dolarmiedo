ab14212c7570aa6224daa353833291af6a2a159e
alemarzano <ajmarzano@gmail.com>
2023-05-03 20:41:59 -0300

    FEAT - Tabla de posiciones con resultados y colores

--- js/app.js
+++ js/app.js
@@ -2,20 +2,24 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 import ScoreComponent from './components/scoreComponent.js';
+import PositionsComponent from './components/positionsComponent.js';
 import { getDolarQuote, coinQuotes } from './components/api.js';
 
 createApp({
   components: {
     score: ScoreComponent,
+    position: PositionsComponent,
   },
   data() {
     return {
       coinQuotes: {},
+      loaded: false,
     };
   },
   mounted() {
     getDolarQuote().then(() => {
       this.coinQuotes = coinQuotes;
+      this.loaded = true;
     });
   },
 }).mount('#app');
--- js/components/positionsComponent.js
+++ js/components/positionsComponent.js
@@ -1,53 +1,111 @@
+import { coinQuotes } from './api.js';
+
+const billetesNacionales = [
+  { nombre: 'Hornero', pesos: 1000 },
+  { nombre: 'Yaguareté', pesos: 500 },
+  { nombre: 'Ballena FA', pesos: 200 },
+  { nombre: 'El Cóndor de los Andes', pesos: 50 },
+  { nombre: 'Guanaco', pesos: 20 },
+  { nombre: 'Dolar Junior', pesos: null },
+  { nombre: 'Dolar Blue', pesos: null },
+];
 export default {
-  template: `<div id="tablapts" class="positions">
-        <div class="titulotabla">Tabla Puntos</div>
-        <table id="positions" class="tablesorter1">
-          <thead>
-            <tr style="background: black; color: white">
-              <th>#</th>
-              <th>Equipo</th>
-              <th class="pts">Pesos</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="punt" name="603">
-              <td class="cp1">1</td>
-              <td align="left"><img />B. Dortmund</td>
-              <td>60</td>
-            </tr>
-            <tr class="pr" name="602">
-              <td class="cp1">2</td>
-              <td align="left"><img />Bayern Munich</td>
-              <td>59</td>
-            </tr>
-            <tr class="ipr" name="943">
-              <td class="cp1">3</td>
-              <td align="left"><img />Union Berlin</td>
-              <td>55</td>
-            </tr>
-            <tr class="pr" name="616">
-              <td class="ds2">16</td>
-              <td align="left"><img />Stuttgart</td>
-              <td>25</td>
-            </tr>
-            <tr class="ipr" name="615">
-              <td class="ds1">17</td>
-              <td align="left"><img />Schalke 04</td>
-              <td>24</td>
-            </tr>
-            <tr class="pr" name="611">
-              <td class="ds1">18</td>
-              <td align="left"><img />Hertha Berlin</td>
-              <td>22</td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="abajotabla">
-          <span class="cp1">-Puesto de Champions League.</span
-          ><span class="cp3">-Puesto de Europa League.</span
-          ><span class="cp4">-Puesto de Liga de Conferencia.</span
-          ><span class="ds1">-Los ultimos 2 equipos descienden.</span
-          ><span class="ds2">-El puesto 16° juega la Promocion.</span>
-        </div>
-      </div>`,
+  data() {
+    return {
+      billetes: billetesNacionales,
+    };
+  },
+  props: ['index', 'nombre', 'pesos'],
+  computed: {
+    dolarJunior() {
+      return this.$root.coinQuotes?.oficial?.value_sell ?? null;
+    },
+    dolarBlue() {
+      return this.$root.coinQuotes?.blue?.value_sell ?? null;
+    },
+  },
+  methods: {
+    updateData() {
+      this.billetes[5].pesos = this.dolarJunior;
+      this.billetes[6].pesos = this.dolarBlue;
+      this.billetes.sort((a, b) => {
+        return b.pesos - a.pesos;
+      });
+    },
+    getClassRow(index) {
+      switch (index) {
+        case 0:
+          return 'bg-green-alert';
+        default:
+          return index % 2 === 0 ? 'bg-grey-light' : 'bg-grey-medium';
+      }
+    },
+    getClassPosition(index) {
+      switch (index) {
+        case 0:
+          return 'bg-green-disclaimer';
+        case this.billetes.length - 2:
+          return 'bg-yellow';
+        case this.billetes.length - 1:
+          return 'bg-alert text-white';
+        default:
+          return null;
+      }
+    },
+    getImage(billete) {
+      switch (billete.nombre) {
+        case 'Dolar Junior':
+          return 'img/oficial.jpg';
+        case 'Dolar Blue':
+          return 'img/blue.jpg';
+        default:
+          return `img/${billete.pesos}.jpg`;
+      }
+    },
+  },
+  watch: {
+    '$root.coinQuotes': {
+      handler: 'updateData',
+      immediate: true,
+    },
+  },
+  template: `
+    <table id="positions" class="positions__table">
+      <thead class="bg-black">
+        <tr class="text-white bg-green-medium">
+          <th colspan="3"><p class="m-0 pt-1 pb-1">Tabla Puntos</p></th>
+        </tr>
+      </thead>
+      <thead class="bg-grey-normal">
+        <tr class="text-white bg-black">
+          <th>#</th>
+          <th>Billetes</th>
+          <th>Pesos</th>
+        </tr>
+      </thead>
+      <tbody class="bg-grey-normal">
+        <tr v-for="(billete, index) in billetes" :key="index" :class="getClassRow(index)">
+          <td class="pr-1 pl-1" align="center" :class="getClassPosition(index)">{{index + 1}}</td>
+          <td align="left" >
+          <div class="flex align-center bold pt-1 pb-1">
+          <img :src="getImage(billete)" class="mr-2 ml-2"/>{{billete.nombre}}</div>
+          </td>
+          <td align="center">{{billete.pesos}}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr class="bg-green-disclaimer text-md">
+        <td colspan="3">
+        <p class="mb-0 mt-0 ml-3">Puestos de convertibilidad</p> 
+        </td>
+        </tr>
+        <tr class="bg-yellow text-md">
+        <td colspan="3">
+        <p class="mb-0 mt-0 ml-3 bold">Jugará la promoción</p> 
+        </td>
+        </tr>
+        <tr class="bg-alert text-white text-md"> <td colspan="3"><p class="mb-0 mt-0 ml-3 bold">El último desciende a moneda metálica</p></td></tr>
+      </tfoot>
+    </table>
+  `,
 };
